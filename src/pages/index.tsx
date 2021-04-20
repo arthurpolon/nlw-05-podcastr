19032a88f4ad4c70ffe9fd628e116fc3e305375e
Arthur Polon <arthurpolon@hotmail.com>
2021-04-20 16:36:45 -0400

    implemented static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
-import { Header } from "../components/Header";
+//SPA = Single Page Application
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, [])
 
-export default function Home() {
+//SSR = Server Side Rendering
+  // export async function getServerSideProps (){
+  //   const response = await fetch('http://localhost:3333/episodes')
+  //   const data = await response.json()
+  //   return {
+  //     props: {
+  //       episodes: data,
+  //     }
+  //   }
+  // }
+
+//SSG = Static Site Generation (in use)
+
+export default function Home(props) {
+  console.log(props.episodes)
   return (
     <h1>Index</h1>
   )
+}
+
+export async function getStaticProps (){
+
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
